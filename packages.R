956092e749125052c6b23b23c4eaa9cbef2e3f34
Ernest Guevarra <ernest@guevarra.io>
2026-07-06 15:16:17 +0100

    add llm packages

--- packages.R
+++ packages.R
@@ -15,3 +15,19 @@ library(openxlsx)
 library(ggplot2)
 library(quarto)
 
+
+## Packages for LLM ----
+library(ellmer)
+library(ollamar)
+
+
+## Packages for testing and validation ----
+library(arsenal)
+
+
+## Packages for reportign and self-publishing ----
+library(rmarkdown)
+library(knitr)
+library(kableExtra)
+library(oxthema)
+library(tidytext)
